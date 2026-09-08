d44c795d1ca119007b0293fc5ccd22d37bc9acd8
rafiahmed0 <rafiahmed1016@gmail.com>
2026-09-08 09:33:29 +0600

    First commit

---
+++ task5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    double pi=3.141592653;
+    double r;
+    cin>>r;
+    cout<<fixed<<setprecision(9)<<pi*r*r<<endl;
+    return 0;
+}
